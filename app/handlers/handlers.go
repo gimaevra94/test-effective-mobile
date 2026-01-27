ead9161dc1c9004855b09f509c90226a5cf8f73a
gimaevra94 <gimaevra94@ya.ru>
2026-01-27 16:53:13 +0200

    JSONErr has been created to send error responses

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -1,7 +1,34 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Subscription struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserId      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+}
+
+type Responce struct {
+	Msg string `json:"msg"`
+}
+
+func JSONErr(w http.ResponseWriter, msg string, statusCode int) {
+	resp := Responce{
+		Msg: msg,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(resp)
+}
 
 func CreateSubscription(w http.ResponseWriter, r *http.Request) {
-	
+	var sub Subscription
+	if err := json.NewDecoder(r.Body).Decode(&sub); err != nil {
+		JSONErr(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
