7ef8637a0085538abd758e6d52599388beb27975
gimaevra94 <gimaevra94@ya.ru>
2026-01-30 10:53:35 +0200

    JSONEerr has been renamed to ErrLogAndResp. ErrLogAndResp modification process has been ended

--- app/errs/errs.go
+++ app/errs/errs.go
@@ -6,16 +6,27 @@ import (
 	"net/http"
 
 	"github.com/gimaevra94/test-effective-mobile/app/structs"
+	"github.com/pkg/errors"
 )
 
-func JSONErr(w http.ResponseWriter, err string, statusCode int) {
+func ErrLogAndResp(w http.ResponseWriter, err error, msg string, statusCode int) {
 	resp := structs.Responce{
-		Msg: err,
+		Msg: msg,
 	}
-
+	
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(resp)
 
-	log.Printf(err)
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
+
+	if stErr, ok := err.(stackTracer); ok {
+		log.Printf("%+v", stErr)
+	} else {
+		err := errors.New(err.Error())
+		stErr := errors.WithStack(err)
+		log.Printf("%+v", stErr)
+	}
 }
--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -2,25 +2,22 @@ package handlers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/gimaevra94/test-effective-mobile/app/database"
 	"github.com/gimaevra94/test-effective-mobile/app/errs"
 	"github.com/gimaevra94/test-effective-mobile/app/structs"
-	"github.com/pkg/errors"
 )
 
 func CreateSubscription(w http.ResponseWriter, r *http.Request) {
 	var sub structs.Subscription
 
 	if err := json.NewDecoder(r.Body).Decode(&sub); err != nil {
-		errs.JSONErr(w, err.Error(), http.StatusBadRequest)
-		log.Printf("%+v", errors.WithStack(err))
+		errs.ErrLogAndResp(w, err, "Bad input", http.StatusBadRequest)
 		return
 	}
 
 	if err := database.CreateSubscription(&sub); err != nil {
-		log.Printf("%+v", err)
+		errs.ErrLogAndResp(w, err, "Internal server error", 500)
 	}
 }
