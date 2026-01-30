c6e9ae199810444796a84a962f8255062ebc82b7
gimaevra94 <gimaevra94@ya.ru>
2026-01-30 17:23:33 +0200

    Add JSON body defer and not null input checking. Modified responce

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -2,7 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gimaevra94/test-effective-mobile/app/database"
 	"github.com/gimaevra94/test-effective-mobile/app/errs"
@@ -11,13 +14,29 @@ import (
 
 func CreateSubscription(w http.ResponseWriter, r *http.Request) {
 	var sub structs.Subscription
-
 	if err := json.NewDecoder(r.Body).Decode(&sub); err != nil {
 		errs.ErrLogAndResp(w, err, "Bad input", http.StatusBadRequest)
 		return
 	}
+	defer r.Body.Close()
+
+	if sub.Price == 0 || sub.ServiceName == "" || sub.StartDate == "" {
+		err := errors.New("some value is empty")
+		errs.ErrLogAndResp(w, err, "some value is empty", http.StatusBadRequest)
+	}
+
+	if _, err := time.Parse("01-2026", sub.StartDate); err != nil {
+		errs.ErrLogAndResp(w, err, "Invalid date format. Use MM-YYYY", http.StatusBadRequest)
+	}
 
 	if err := database.CreateSubscription(&sub); err != nil {
 		errs.ErrLogAndResp(w, err, "Internal server error", 500)
+		return
 	}
+
+	location := fmt.Sprintf("/api/v1/subscriptions/%s_%s", sub.UserId, sub.ServiceName)
+	w.Header().Set("Location", location)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(sub)
 }
