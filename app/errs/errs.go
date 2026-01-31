335d698c77efb9d3d6abca7191d49df4965603b2
gimaevra94 <gimaevra94@ya.ru>
2026-01-31 12:51:50 +0200

    Func ErrLogAndResp updated

--- app/errs/errs.go
+++ app/errs/errs.go
@@ -7,32 +7,18 @@ import (
 	"net/http"
 
 	"github.com/gimaevra94/test-effective-mobile/app/structs"
-	"github.com/pkg/errors"
 )
 
 // Функция предоставляет JSON ответ клиенту в случае ошибки и логирует ее.
 // Принимает на вход Response для записи сообщения об ошибке, http статус
 // И саму ошибку для логирования.
-// Если ошибка приходит без трассировки функция добавляет к ошибке трассирвоку
-// перед логированием.
 func ErrLogAndResp(w http.ResponseWriter, err error, msg string, statusCode int) {
 	resp := structs.Responce{
 		Msg: msg,
 	}
-	
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(resp)
-
-	type stackTracer interface {
-		StackTrace() errors.StackTrace
-	}
-
-	if stErr, ok := err.(stackTracer); ok {
-		log.Printf("%+v", stErr)
-	} else {
-		err := errors.New(err.Error())
-		stErr := errors.WithStack(err)
-		log.Printf("%+v", stErr)
-	}
+	log.Printf("%+v", err)
 }
