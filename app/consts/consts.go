757048b0da1be6c1e9ee32fdc5aa695dc4f35db3
gimaevra94 <gimaevra94@ya.ru>
2026-01-31 19:38:52 +0200

    added InsertQuery, SelectQuery

--- app/consts/consts.go
+++ app/consts/consts.go
@@ -1,9 +1,16 @@
 package consts
 
+// For api handlers
 const (
 	MethodNotAllowed    = "Method not allowed"
 	BadInput            = "Bad input"
 	EmptyValue          = "Empty value"
 	InvalidDate         = "Invalid date. Use MM-YYYY"
 	InternalServerError = "Internal server error"
+)
+
+// SQL requests
+const (
+	InsertQuery = "insert into subscriptions (service_name, price, user_id, start_date) values ($1, $2, $3, $4)"
+	SelectQuery = "select * where user_id = ? and service_name = ? inline 1"
 )
