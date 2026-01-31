ccbf50305aa2d9e0d279df188738eafbfbbc507a
gimaevra94 <gimaevra94@ya.ru>
2026-01-31 17:53:54 +0200

    создал файл с константами и добвил в него несколько констант

---
+++ app/consts/consts.go
@@ -0,0 +1,9 @@
+package consts
+
+const (
+	MethodNotAllowed    = "Method not allowed"
+	BadInput            = "Bad input"
+	EmptyValue          = "Empty value"
+	InvalidDate         = "Invalid date. Use MM-YYYY"
+	InternalServerError = "Internal server error"
+)
