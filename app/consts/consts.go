7748f9c17731d6df3e767004858cf78ef67b69eb
gimaevra94 <gimaevra94@ya.ru>
2026-02-02 09:55:32 +0200

    Spelling mistakes for api handlers was fixed. Select query was fixed

--- app/consts/consts.go
+++ app/consts/consts.go
@@ -3,14 +3,15 @@ package consts
 // For api handlers
 const (
 	MethodNotAllowed    = "Method not allowed"
-	BadInput            = "Bad input"
-	EmptyValue          = "Empty value"
+	BadInput            = "Incorrect input"
+	EmptyValue          = "The input has an empty value"
 	InvalidDate         = "Invalid date. Use MM-YYYY"
+	NotExist            = "The row does not exist"
 	InternalServerError = "Internal server error"
 )
 
 // SQL requests
 const (
 	InsertQuery = "insert into subscriptions (service_name, price, user_id, start_date) values ($1, $2, $3, $4)"
-	SelectQuery = "select * where user_id = ? and service_name = ? inline 1"
+	SelectQuery = "select service_name, price, user_id, start_date from subscriptions where user_id = ? and service_name = ?"
 )
