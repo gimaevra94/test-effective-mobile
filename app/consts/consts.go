819f94fa8d195545c1d0b98ed70df717927c5137
gimaevra94 <gimaevra94@ya.ru>
2026-02-04 10:58:45 +0200

    Spelling mistakes for SQL querys was fixed. DeleteQuery has been added

--- app/consts/consts.go
+++ app/consts/consts.go
@@ -14,8 +14,9 @@ const (
 // For SQL Requests
 const (
 	InsertQuery = "insert into subscription (" + ServiceName + ", " + Price + ", " + UserID + ", " + StartDate + ") values ($1, $2, $3, $4)"
-	SelectQuery = "select " + ServiceName + ", " + Price + ", " + UserID + ", " + StartDate + "from subscription where " + UserID + " = $1 and " + ServiceName + " = $2"
-	UpdateQuery = "update subscription set " + Price + "= $1 where " + UserID + "= $2 and " + ServiceName + "= $3"
+	SelectQuery = "select " + ServiceName + ", " + Price + ", " + UserID + ", " + StartDate + "from subscription where " + ServiceName + " = $1 and " + UserID + " = $2"
+	UpdateQuery = "update subscription set " + Price + " = $1 where " + ServiceName + " = $2 and " + UserID + " = $3 returning " + ServiceName + ", " + Price + ", " + UserID + ", " + StartDate
+	DeleteQuery = "delete from subscription where " + ServiceName + " = $1 and " + UserID + " = &2"
 )
 
 // Path values
