800fdc7210e8c5ea59a9f2c53b114c84caf0ccaf
gimaevra94 <gimaevra94@ya.ru>
2026-02-04 19:40:14 +0200

    The 'gorm' tags have been added to 'Subscription' struct.

--- app/structs/structs.go
+++ app/structs/structs.go
@@ -1,10 +1,10 @@
 package structs
 
 type Subscription struct {
-	ServiceName string `json:"service_name"`
-	Price       int    `json:"price"`
-	UserID      string `json:"user_id"`
-	StartDate   string `json:"start_date"`
+	ServiceName string `json:"service_name" gorm:"column:service_name"`
+	Price       int    `json:"price" gorm:"column:price"`
+	UserID      string `json:"user_id" gorm:"column:user_id"`
+	StartDate   string `json:"start_date" gorm:"column:start_date"`
 }
 
 type Responce struct {
